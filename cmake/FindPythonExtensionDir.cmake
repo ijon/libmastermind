f151c85bd945a1f04d8d2d14b4260b9717782d7f
Ivan Chelubeev <ivan.chelubeev@gmail.com>
2016-07-20 18:57:01 +0300

    work-in-progress: build python extension wheel, and install from it

--- cmake/FindPythonExtensionDir.cmake
+++ cmake/FindPythonExtensionDir.cmake
@@ -1,4 +1,13 @@
-execute_process(COMMAND "${PYTHON_EXECUTABLE}" -c "from distutils import sysconfig; print sysconfig.get_python_lib(prefix='${CMAKE_INSTALL_PREFIX}'),"
-	RESULT_VARIABLE ret_var
+execute_process(COMMAND "${PYTHON_EXECUTABLE}" -c "from distutils import sysconfig; print sysconfig.get_python_lib(prefix='${CMAKE_INSTALL_PREFIX}')"
+	RESULT_VARIABLE _FAILED
 	OUTPUT_VARIABLE PYTHON_EXTENSION_DIR
-	OUTPUT_STRIP_TRAILING_WHITESPACE)
+	OUTPUT_STRIP_TRAILING_WHITESPACE
+)
+if (_FAILED)
+    message(FATAL_ERROR "Can't python packages directory")
+endif()
+
+#TODO: describe the reason
+string(REGEX REPLACE "site-packages$" "dist-packages" PYTHON_EXTENSION_DIR "${PYTHON_EXTENSION_DIR}")
+
+unset(_FAILED)
